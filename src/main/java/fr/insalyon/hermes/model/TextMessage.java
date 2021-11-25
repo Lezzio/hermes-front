ef84cecf6033aa04d9de163b663427b533353ebd
bdeleglise <benoit.deleglise@outlook.fr>
2021-11-25 14:39:19 +0100

    little fix

--- src/main/java/fr/insalyon/hermes/model/TextMessage.java
+++ src/main/java/fr/insalyon/hermes/model/TextMessage.java
@@ -6,12 +6,23 @@ public class TextMessage extends Message {
 
     private final String content;
 
+    private boolean isSpecialEvent;
+
     public TextMessage(String content, String sender, String destination, Date time) {
         super(sender, destination, time);
         this.content = content;
+        this.isSpecialEvent = false;
     }
 
     public String getContent() {
         return content;
+    }
+
+    public void setSpecialEvent(){
+        this.isSpecialEvent = true;
+    }
+
+    public boolean isSpecialEvent() {
+        return this.isSpecialEvent;
     }
 }
